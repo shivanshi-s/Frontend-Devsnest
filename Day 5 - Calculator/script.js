336180b6e1c0968294df14a45d1e948809634fa4
shivanshi <shivanshi.saxena2@gmail.com>
2021-06-11 19:14:12 +0530

    calculator

---
+++ Day 5 - Calculator/script.js
@@ -0,0 +1,8 @@
+function sumValues()
+{
+    var num1,num1,res;
+    num1=Number(document.formcalc.txtnum1.value);
+    num2=Number(document.formcalc.txtnum2.value);
+    res = num1+num2;
+    document.formcalc.txtres.value=res;
+}
